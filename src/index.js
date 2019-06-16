933f925f9c11ace8dd7f171b2050b9eed95d3385
Oscar Tunebing <tunebing@gmail.com>
2019-06-16 15:27:32 +0200

    Initial version

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { findUrl, mongo } = require('@nam3/oscar-util');
+
+const app = express();
+
+// Get enviorment variables.
+const {
+    PORT,
+    MONGO_URI,
+} = process.env;
+
+const connection = mongo(MONGO_URI);
+
+app.get('/:hash', asyncHandler(async (req, res) => {
+    await connection(async db => {
+        const url = await findUrl(db, req.params.hash);
+        if(url) {
+            res.redirect(url);
+        } else {
+            res.redirect('/app');
+        }
+    });
+}));
+
+// Start server.
+app.listen(PORT, () => {
+    console.log(`Server listening on: ${PORT}`);
+});
